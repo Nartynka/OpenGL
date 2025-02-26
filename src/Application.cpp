3f118a3c05bff2e48bba1ba749f4bb69fce0beac
Nartyna <mplutwska@gmail.com>
2025-02-26 23:14:09 +0100

    Add more comments, add vertex array object (vao)
    new background color and new rect color change

--- src/Application.cpp
+++ src/Application.cpp
@@ -120,6 +120,11 @@ int main()
 	if (!glfwInit())
 		return -1;
 
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "HEHEHE", NULL, NULL);
 	if (!window)
@@ -131,13 +136,15 @@ int main()
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	// "vsync"
 	glfwSwapInterval(1);
 
 	if (glewInit() != GLEW_OK)
 		std::cout << "ERROR!!" << std::endl;
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
-
+	
+	// vertices positions
 	float positions[] = {
 		-0.5f, -0.5f,
 		 0.5f, -0.5f,
@@ -145,6 +152,7 @@ int main()
 		-0.5f, 0.5f,
 	};
 
+	// indices of positions of vertices of two triangles that will make a rectangle
 	// counterclockwise drawing
 	unsigned int indices[]
 	{
@@ -152,19 +160,27 @@ int main()
 		2, 3, 0
 	};
 
+	// vertex array object
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
+	// Buffer for our positions
 	unsigned int buffer;
 	glGenBuffers(1, &buffer); // create a buffer
-	glBindBuffer(GL_ARRAY_BUFFER, buffer); // "select" buffer
+	glBindBuffer(GL_ARRAY_BUFFER, buffer); // if we want to change something about the buffer, we have to first "select it" aka. bind it
+	// 4 * 2 -> 4 vertices, 2 floats each
 	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	// vertex attribute that will be passed into the vertex shader
 	// we have only one attribute - position of a single vertex that is represented by two floats
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	// this line actually binds/links the current bind buffer to the vao
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // structure of a vertex buffer (each vertex consists of 2 floats and etc.)
 
-
-	unsigned int ibo; // index buffer object
+	// we use index buffer to eliminate the need to store some vertices twice 
+	// (rectangle is made of to triangles next to each other so two vertices would be stored twice and that is not good for memory)
+	unsigned int ibo; // index/element buffer object
 	glGenBuffers(1, &ibo); // create a buffer
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo); // "select" buffer
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
@@ -178,33 +194,45 @@ int main()
 	int location = glGetUniformLocation(shader, "u_Color");
 
 	float r = 0.0f;
-	float g = 1.0f;
-	float increment_r = 0.01f;
-	float increment_g = 0.01f;
+	float increment = 0.01f;
+	
+	// clear all gl states
+	glBindVertexArray(0); // vao stores binding and unbinding so if we want to just clear states, we have to unbind vao first. Otherwise vao will store the unbinds and we will have to bind everything again before draw.
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);	
+	glUseProgram(0);
+	// "wire frame mode"
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
+		glClearColor(0.5f, 0.7f, 0.8f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUniform4f(location, r, g, 0.4f, 1.f);
+		// vao stores the binds so we don't have to bind them here, just binding the vao will suffice
+		//glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+		glBindVertexArray(vao);
+		glUseProgram(shader);
+	
+		float time = glfwGetTime();
+		float g = sin(time) / 2.0f + 0.5f;
+
+		glUniform4f(location, r, g, 0.8f, 1.f);
+
 		// Draw the triangle
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
-
 		if (r > 1.0f)
-			increment_r = -0.01f;
+			increment = -0.01f;
 		else if (r < 0.0f)
-			increment_r = 0.01f;
+			increment = 0.01f;
 
-		if (g > 1.0f)
-			increment_g = -0.01f;
-		else if (g < 0.0f)
-			increment_g = 0.01f;
+		r += increment;
 
-		r += increment_r;
-		g += increment_g;
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
